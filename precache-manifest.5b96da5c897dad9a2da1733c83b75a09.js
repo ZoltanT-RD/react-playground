30af9c7fb3a2900020eba08c0a42b0b6b866749b
Zoltan Tompa <zoltan@resdiary.com>
2020-09-04 15:38:19 +0100

    Updates

---
+++ precache-manifest.5b96da5c897dad9a2da1733c83b75a09.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9629d52b3b2e757560c9a075368320ac",
+    "url": "/react-playground/index.html"
+  },
+  {
+    "revision": "4031b2743b035a886d36",
+    "url": "/react-playground/static/css/2.a2f31877.chunk.css"
+  },
+  {
+    "revision": "d6ad59c2044c321d9b27",
+    "url": "/react-playground/static/css/main.5ae75eb8.chunk.css"
+  },
+  {
+    "revision": "4031b2743b035a886d36",
+    "url": "/react-playground/static/js/2.817661d3.chunk.js"
+  },
+  {
+    "revision": "eb94d6017d8f0c633f4acf9346a3ecc8",
+    "url": "/react-playground/static/js/2.817661d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6ad59c2044c321d9b27",
+    "url": "/react-playground/static/js/main.2629f2fc.chunk.js"
+  },
+  {
+    "revision": "a0384384110ee4cae2cf",
+    "url": "/react-playground/static/js/runtime-main.7942e741.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-playground/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-playground/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-playground/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-playground/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-playground/static/media/slick.f97e3bbf.svg"
+  }
+]);
